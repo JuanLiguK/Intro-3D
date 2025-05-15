42d5935dc67f12eb3bd7fa4c8bf489272ee529bb
JuanLiguK <bl0016@est.ort.edu.ar>
2025-05-15 12:09:00 -0300

    Terminado

--- Assets/Scrips/ClonaCubos.cs
+++ Assets/Scrips/ClonaCubos.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ClonaCubos : MonoBehaviour
 {
+    public GameObject cuboPreFab;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,10 @@ public class ClonaCubos : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ClonarCubo()
+    {
+        Instantiate(cuboPreFab);
     }
 }
